274dc4538aca1a7fd0099ad9bbf2f1d82b45e3f3
ti.eugene <ti.eugene@bd074bf9-a489-2ae5-b4c1-63aac862ad04>
2013-06-18 10:34:28 +0000

    UI tunes

--- trunk/doxgen/views.py
+++ trunk/doxgen/views.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
+from django.shortcuts import redirect
 from django.views.generic.simple import direct_to_template
 from django.conf import settings
 
 def	index(request):
-	return direct_to_template(request, 'index.html')
+	return redirect('dox.views.index')
+	#return direct_to_template(request, 'index.html')
 
 def	about(request):
 	return direct_to_template(request, 'about.html')
