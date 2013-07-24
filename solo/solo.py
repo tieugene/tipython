ccffa6044407d339f5e72075f094c0846cba99a1
ti.eugene <ti.eugene@bd074bf9-a489-2ae5-b4c1-63aac862ad04>
2013-07-24 10:31:03 +0000

    ready to debug

--- solo/solo.py
+++ solo/solo.py
@@ -4,10 +4,17 @@
 example: http://ip-nalog.ru/forma-p21001-1.html
 TODO:
 	* set okveds on error
-	* addr_*_type - dropdowns:
-	-- корпус, строение, лит
-	-- квартира, помещение, офис
+	* addr_*_type - dropdowns
 	* addr_locality - dropdown (http://www.mk-kadar.ru/service/ipdocs.htm)
+Кладр:
+1. srf
+2. район: р-н, тер, у
+3. город (волость)
+4. нп
+5. ул
+6. дом: дом
+7. корпус, строение, лит
+8. квартира, помещение, офис
 '''
 
 # 3rd parties
@@ -22,7 +29,7 @@ debug = True
 cache = False
 #forward_url = 'http://localhost/doxgen/doxgen/'
 forward_url = 'http://dox.eap.su/doxgen/doxgen/'
-token = '59aeb29436ed7c3328ff58dd46ba5b0a'
+token = 'fb5168457a495de80c5d7f18205740c2'
 try:
         from local_settings import *
 except ImportError:
@@ -50,42 +57,131 @@ todo_list = [
 	('2', 'Выдать заявителю или лицу, действующему на основании доверенности'),
 	('3', 'Отправить по почте'),
 ]
-location_list = [
-	('1',  'Александровская (Курортный район), пос.'),
-	('2',  'Александровская (Пушкинский район), пос.'),
-	('3',  'Белоостров, пос.'),
-	('4',  'Володарская, ст.'),
-	('5',  'Горелово, пос.'),
-	('6',  'Горская, ст.'),
-	('7',  'Комарово, пос.'),
-	('8',  'Лахта, пос.'),
-	('9',  'Левашово, пос.'),
-	('10', 'Лисий Нос, пос.'),
-	('11', 'Металлострой, пос.'),
-	('12', 'Можайская, ст.'),
-	('13', 'Молодежное, пос.'),
-	('14', 'Ольгино, пос.'),
-	('15', 'Парголово, пос.'),
-	('16', 'Песочный, пос.'),
-	('17', 'Петро-Славянка, пос.'),
-	('18', 'Понтонный, пос.'),
-	('19', 'Разлив, ст.'),
-	('20', 'Репино, пос.'),
-	('21', 'Саперный, пос.'),
-	('22', 'Серово, пос.'),
-	('23', 'Смолячково, пос.'),
-	('24', 'Солнечное, пос.'),
-	('25', 'Старо-Паново, дер.'),
-	('26', 'Стрельна, пос.'),
-	('27', 'Тарховка, пос.'),
-	('28', 'Торики, дер.'),
-	('29', 'Тярлево, пос.'),
-	('30', 'Усть-Ижора, пос.'),
-	('31', 'Ушково, пос.'),
-	('32', 'Шушары, пос.'),
+locality_list = [
+	'',
+	'Александровская (Курортный район), пос.',
+	'Александровская (Пушкинский район), пос.',
+	'Белоостров, пос.',
+	'Володарская, ст.',
+	'Горелово, пос.',
+	'Горская, ст.',
+	'Комарово, пос.',
+	'Лахта, пос.',
+	'Левашово, пос.',
+	'Лисий Нос, пос.',
+	'Металлострой, пос.',
+	'Можайская, ст.',
+	'Молодежное, пос.',
+	'Ольгино, пос.',
+	'Парголово, пос.',
+	'Песочный, пос.',
+	'Петро-Славянка, пос.',
+	'Понтонный, пос.',
+	'Разлив, ст.',
+	'Репино, пос.',
+	'Саперный, пос.',
+	'Серово, пос.',
+	'Смолячково, пос.',
+	'Солнечное, пос.',
+	'Старо-Паново, дер.',
+	'Стрельна, пос.',
+	'Тарховка, пос.',
+	'Торики, дер.',
+	'Тярлево, пос.',
+	'Усть-Ижора, пос.',
+	'Ушково, пос.',
+	'Шушары, пос.',
 ]
-#	-- корпус, строение, лит
-#	-- квартира, помещение, офис
+street_type_list = [
+	('б-р', 'Бульвар'),
+	('въезд', 'Въезд'),
+	('дор', 'Дорога'),
+	('жт', 'Животноводческая точка'),
+	('заезд', 'Заезд'),
+	('кв-л', 'Квартал'),
+	('км', 'Километр'),
+	('кольцо', 'Кольцо'),
+	('линия', 'Линия'),
+	('наб', 'Набережная'),
+	('остров', 'Остров'),
+	('парк', 'Парк'),
+	('пер', 'Переулок'),
+	('переезд', 'Переезд'),
+	('пл', 'Площадь'),
+	('пл-ка', 'Площадка'),
+	('проезд', 'Проезд'),
+	('пр-кт', 'Проспект'),
+	('просек', 'Просек'),
+	('проселок', 'Проселок'),
+	('проулок', 'Проулок'),
+	('сад', 'Сад'),
+	('сквер', 'Сквер'),
+	('стр', 'Строение'),
+	('тер', 'Территория'),
+	('тракт', 'Тракт'),
+	('туп', 'Тупик'),
+	('ул', 'Улица'),
+	('уч-к', 'Участок'),
+	('ш', 'Шоссе'),
+	('высел', 'Выселки(ок)'),
+	('городок', 'Городок'),
+	('д', 'Деревня'),
+	('ж/д_будка', 'Железнодорожная будка'),
+	('ж/д_казарм', 'Железнодорожная казарма'),
+	('ж/д_оп', 'ж/д останов. (обгонный) пункт'),
+	('ж/д_пост', 'Железнодорожный пост'),
+	('ж/д_рзд', 'Железнодорожный разъезд'),
+	('ж/д_ст', 'Железнодорожная станция'),
+	('казарма', 'Казарма'),
+	('м', 'Местечко'),
+	('мкр', 'Микрорайон'),
+	('нп', 'Населенный пункт'),
+	('платф', 'Платформа'),
+	('п', 'Поселок'),
+	('п/о', 'Почтовое отделение'),
+	('п/р', 'Планировочный район'),
+	('п/ст', 'Поселок и(при) станция(и)'),
+	('полустанок', 'Полустанок'),
+	('починок', 'Починок'),
+	('рзд', 'Разъезд'),
+	('с', 'Село'),
+	('сл', 'Слобода'),
+	('ст', 'Станция'),
+	('х', 'Хутор'),
+	('ж/д_платф', 'Железнодорожная платформа'),
+	('спуск', 'Спуск'),
+	('канал', 'Канал'),
+	('гск', 'Гаражно-строительный кооперат'),
+	('снт', 'Садовое неком-е товарищество'),
+	('лпх', 'Леспромхоз'),
+	('проток', 'Проток'),
+	('коса', 'Коса'),
+	('вал', 'Вал'),
+	('ферма', 'Ферма'),
+	('мост', 'Мост'),
+	('ряды', 'Ряды'),
+	('а/я', 'Абонентский ящик'),
+	('берег', 'Берег'),
+	('просека', 'Просека'),
+	('протока', 'Протока'),
+	('бугор', 'Бугор'),
+	('зона', 'Зона'),
+	('днп', 'Дачное неком-е партнерство'),
+	('н/п', 'Некоммерческое партнерство'),
+	('ф/х', 'Фермерское хозяйство'),
+]
+building_type_list = [
+	('', '---'),
+	('корп', 'корпус'),
+	('стр', 'строение'),
+	('лит', 'литер'),
+]
+app_type_list = [
+	('', '---'),
+	('кв', 'квартира'),
+	('пом', 'помещение'),
+	('оф', 'офис'),
+]
 # validators
 chk_empty = web.form.Validator('Обязательное поле', bool)
 chk_date = web.form.regexp('^(3[01]|[12][0-9]|0[1-9])\.(1[0-2]|0[1-9])\.[0-9]{4}$', 'Не похоже на дату (ДД.ММ.ГГГГ)')
@@ -137,16 +233,14 @@ ip_form = web.form.Form(
 	web.form.Textbox('birthplace',		chk_empty, description='Место рождения', maxlength='80'),
 	web.form.Textbox('inn',			ChkInn(), description='ИНН', size='12'),
 	web.form.Textbox('addr_zip',		chk_empty, chk_6, description='Индекс', size='6'),	# minlength, maxlength
-	web.form.Textbox('addr_locality_type',	description='Населенный пункт.Тип', maxlength='10'),
-	web.form.Textbox('addr_locality_name',	description='Населенный пункт.Наименование', maxlength='68'),
-	web.form.Textbox('addr_street_type',	chk_empty, description='Улица.Тип', maxlength='10'),
+	web.form.Dropdown('addr_locality',	description='Населенный пункт', args=locality_list),
+	web.form.Dropdown('addr_street_type',	description='Улица.Тип', args=street_type_list, value='ул'),
 	web.form.Textbox('addr_street_name',	chk_empty, description='Улица.Наименование', maxlength='68'),
-	web.form.Textbox('addr_house_type',	chk_empty, description='Дом.Тип', maxlength='10'),
-	web.form.Textbox('addr_house_name',	chk_empty, description='Дом.Номер', maxlength='8'),
-	web.form.Textbox('addr_building_type',	description='Корпус (строение).Тип', maxlength='10'),
-	web.form.Textbox('addr_building_name',	description='Корпус (строение).Номер', maxlength='8'),
-	web.form.Textbox('addr_app_type',	description='Квартира (офис, помещение).Тип', maxlength='8'),
-	web.form.Textbox('addr_app_name',	description='Квартира (офис, помещение).Номер', maxlength='8'),
+	web.form.Textbox('addr_house_name',	chk_empty, description='Дом', maxlength='8'),
+	web.form.Dropdown('addr_building_type',	description='Корпус.Тип', args=building_type_list),
+	web.form.Textbox('addr_building_name',	description='Корпус.Номер', maxlength='8'),
+	web.form.Dropdown('addr_app_type',	description='Квартира.Тип', args=app_type_list),
+	web.form.Textbox('addr_app_name',	description='Квартира.Номер', maxlength='8'),
 	web.form.Textbox('doc_series',		chk_empty, chk_4, description='Серия', size='4'),
 	web.form.Textbox('doc_no',		chk_empty, chk_6, description='Номер', size='6'),
 	web.form.Textbox('doc_date',		chk_empty, chk_date, ChkDate(), description='Дата', size='10'),
@@ -155,20 +249,28 @@ ip_form = web.form.Form(
 	web.form.Textbox('phone_code',		description='Код', size='5'),
 	web.form.Textbox('phone_no',		description='Телефон', size='7'),
 	web.form.Textbox('email',		description='E-mail'),
-	web.form.Dropdown('todo',		description='Что делать', args=todo_list, value='1'),
+	web.form.Dropdown('todo',		description='Что делать с документами', args=todo_list, value='1'),
 	web.form.Dropdown('tax',		description='Налогообложение', args=tax_list),
 	#web.form.Checkbox('selected',		description='ОКВЭДы'),
 	validators = [web.form.Validator('Добавьте хотя бы один ОКВЭД', lambda i: len(web.input(selected=[]).selected))]
 )
 
 def	prepare_21001(f, addr, selected):
+	locality = f.addr_locality.get_value()
+	if locality:
+		locality = locality.rsplit(' ', 1)
+		ltype = locality[0].rstrip(',')
+		lname = locality[1].rstrip('.')
+	else:
+		ltype = ''
+		lname = ''
 	retvalue = {
 		'csrfmiddlewaretoken':	token,
 		'_action':		'print',
 		'lastname':		f.lastname.get_value(),
 		'firstname':		f.firstname.get_value(),
 		'midname':		f.midname.get_value(),
-		'sex':			('мужской', 'женский')[int(f.sex.get_value())-1],
+		'sex':			f.sex.get_value(),
 		'birthdate':		f.birthdate.get_value(),
 		'birthplace':		f.birthplace.get_value(),
 		'citizenship':		'1',
@@ -176,23 +278,23 @@ def	prepare_21001(f, addr, selected):
 		'addr_srf':		'78',
 		'addr_city_type':	'г',
 		'addr_city_name':	'Санкт-Петербург',
-		'addr_locality_type':	f.addr_locality_type.get_value(),
-		'addr_locality_name':	f.addr_locality_name.get_value(),
+		'addr_locality_type':	ltype,
+		'addr_locality_name':	lname,
 		'addr_street_type':	f.addr_street_type.get_value(),
 		'addr_street_name':	f.addr_street_name.get_value(),
-		'addr_house_type':	f.addr_house_type.get_value(),
+		'addr_house_type':	'д',
 		'addr_house_name':	f.addr_house_name.get_value(),
 		'addr_building_type':	f.addr_building_type.get_value(),
 		'addr_building_name':	f.addr_building_name.get_value(),
 		'addr_app_type':	f.addr_app_type.get_value(),
 		'addr_app_name':	f.addr_app_name.get_value(),
-		'phone':		'+7 %s %s' % (f.phone_code.get_value(), f.phone_no.get_value()),
 		'doc_type':		21,
 		'doc_series_no':	f.doc_series.get_value() + ' ' + f.doc_no.get_value(),
 		'doc_date':		f.doc_date.get_value(),
 		'doc_whom':		f.doc_who.get_value(),
 		'doc_kp':		f.doc_kp.get_value(),
 		'inn':			f.inn.get_value(),
+		'phone':		'+7 %s %s' % (f.phone_code.get_value(), f.phone_no.get_value()),
 		'email':		f.email.get_value(),
 		'todo':			f.todo.get_value(),
 		'okved-TOTAL_FORMS':	len(selected),
@@ -201,6 +303,7 @@ def	prepare_21001(f, addr, selected):
 	}
 	for i, v in enumerate(selected):
 		retvalue['okved-%d-code' % i] = v.replace('_', '.').lstrip('a')
+	pprint.pprint(retvalue)
 	return retvalue
 
 def	prepare_pd4(f, addr, selected):
@@ -264,11 +367,14 @@ class	index:
 			return render.form(f, get_okveds(), selected_list)
 		else:
 			# FIXME:
-			addr = \
-				f.addr_zip.get_value() + \
-				', г. Санкт-Петербург, ' + \
-				f.addr_street_type.get_value() + '. ' + f.addr_street_name.get_value() + ', ' + \
-				f.addr_house_type.get_value() + ' ' + f.addr_house_name.get_value()
+			addr = f.addr_zip.get_value() + ', г. Санкт-Петербург, '
+			if (f.addr_locality.get_value()):
+				addr = addr + f.addr_locality.get_value() + ', '
+			addr = addr + f.addr_street_type.get_value() + '. ' + f.addr_street_name.get_value() + ', д. ' + f.addr_house_name.get_value()
+			if (f.addr_building_type.get_value()):
+				addr = addr + ', ' + f.addr_building_type.get_value() + '. ' + f.addr_building_name.get_value()
+			if (f.addr_app_type.get_value()):
+				addr = addr + ', ' + f.addr_app_type.get_value() + '. ' + f.addr_app_name.get_value()
 			output = pyPdf.PdfFileWriter()
 			error = False
 			tmpfile = list()
@@ -278,6 +384,7 @@ class	index:
 			for key, func in tocall:
 				url = forward_url + forms[key] + '/a/'
 				r = requests.post(url, data=func(f, addr, selected), cookies=dict(csrftoken=token))
+				print url, r.status_code
 				if (r.status_code == 200):	# r - Responce object
 					if (r.headers['content-type'] == 'application/pdf'):
 						tmp = tempfile.NamedTemporaryFile(suffix='.pdf', delete=False)
