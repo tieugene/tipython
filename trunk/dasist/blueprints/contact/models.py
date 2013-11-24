ecbdf12d7ee300701b8ca3584533b73db9d4f0a6
ti.eugene <ti.eugene@bd074bf9-a489-2ae5-b4c1-63aac862ad04>
2013-11-24 18:44:25 +0000

    dasist: multiple addresses ok

--- trunk/dasist/blueprints/contact/models.py
+++ trunk/dasist/blueprints/contact/models.py
@@ -1,6 +1,13 @@
 # -*- coding: UTF-8 -*-
+'''
+http://pythonhosted.org/Flask-SQLAlchemy/models.html
+http://docs.sqlalchemy.org/en/latest/orm/relationships.html
+http://docs.formalchemy.org
+http://flask.pocoo.org/mailinglist/archive/2012/5/11/flask-wtf-problems-with-csrf-and-formfield/
+http://stackoverflow.com/questions/18188428/dynamic-forms-formsets-in-flask-wtforms
+http://stackoverflow.com/questions/9885693/how-i-do-to-update-data-on-many-to-many-with-wtforms-and-sqlalchemy
+'''
 __author__ = 'ti.eugene@gmail.com'
-
 
 from dasist import db
 from datetime import datetime
@@ -11,6 +18,16 @@ class Contact(db.Model):
     lastname	= db.Column(db.String(length=64))
     midname	= db.Column(db.String(length=64))
     birthdate	= db.Column(db.DateTime())
+    addresses	= db.relationship('Address', backref='contact', lazy='dynamic')
 
     def __unicode__(self):
         return '%s %s %s' % (self.lastname, self.firstname, self.midname)
+
+class AddressType(db.Model):
+    id		= db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name	= db.Column(db.String(length=16))
+
+class Address(db.Model):
+    id		= db.Column(db.Integer, autoincrement=True, primary_key=True)
+    addr	= db.Column(db.String(length=254))
+    contact_id	= db.Column(db.Integer, db.ForeignKey('contact.id'))
