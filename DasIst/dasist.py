ec8f21e24909a068d857f8238a64e8ab4244c63e
ti.eugene <ti.eugene@bd074bf9-a489-2ae5-b4c1-63aac862ad04>
2013-11-24 13:05:19 +0000

    1st run ok

--- DasIst/dasist.py
+++ DasIst/dasist.py
@@ -3,33 +3,30 @@
 # Description:
 __author__ = 'ti.eugene@gmail.com'
 
-from flask import Flask
-app = Flask(__name__)
-app.config.from_object('config')
+import os, sys
 
+#PROJECT_DIR = os.path.dirname(__file__)
+#sys.path.append(PROJECT_DIR)
+
+import flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
+app = flask.Flask(__name__)
+app.config.from_object('config')
 db = SQLAlchemy(app)
 
-from blueprints.contacts.views import contacts as contactsModule
-app.register_blueprint(contactsModule)
+from blueprints.contacts.views import contacts
+app.register_blueprint(contacts)
 
 @app.errorhandler(404)
 def not_found(error):
-    return render_template('404.html'), 404
+    return flask.render_template('404.html'), 404
 
 
 @app.route('/')
-@app.route('/index')
 def index():
-    """Just a generic index page to show."""
-    return render_template('index.html')
+    return flask.render_template('index.html')
 
 # 1. standalone
 if __name__ == '__main__':
-	application = web.application(('/', 'index'), globals())
-	application.internalerror = web.debugerror
-	application.run()
-# 2. apache mod_wsgi
-os.chdir(os.path.dirname(__file__))
-application = web.application(('/', 'index'), globals()).wsgifunc()
+	app.run()
