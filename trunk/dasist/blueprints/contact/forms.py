288c82ff4b3366b3fc1bc77c1c020aab6b797f48
ti.eugene <ti.eugene@bd074bf9-a489-2ae5-b4c1-63aac862ad04>
2013-11-25 08:19:51 +0000

    flush

--- trunk/dasist/blueprints/contact/forms.py
+++ trunk/dasist/blueprints/contact/forms.py
@@ -17,13 +17,12 @@ from datetime import datetime
 class AddressForm(Form):
 	addr = wtf.StringField(u'Адрес',validators=[validators.Length(max=254)])
 
+	def __init__(self, csrf_enabled=False, *args, **kwargs):
+		super(FilterForm, self).__init__(csrf_enabled=csrf_enabled, *args, **kwargs)
+
 class ContactForm(Form):
 	lastname = wtf.StringField(u'Фамилия',validators=[validators.Length(max=64)])
 	firstname = wtf.StringField(u'Имя',validators=[Required(),validators.Length(max=64)])
 	midname = wtf.StringField(u'Отчество',validators=[validators.Length(max=64)])
 	birthdate = wtf.DateField(u'Дата рождения')
 	addresses = wtf.FieldList(wtf.FormField(AddressForm))
-
-def __init__(self, *args, **kwargs):
-	kwargs['csrf_enabled'] = False
-	super(ContactForm, self).__init__(*args, **kwargs)
