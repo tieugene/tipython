68fba3406f368a6d359876c2d447e9847a1e0f31
ti.eugene <ti.eugene@bd074bf9-a489-2ae5-b4c1-63aac862ad04>
2013-11-26 13:20:11 +0000

    flush

--- dasist/blueprints/contact/forms.py
+++ dasist/blueprints/contact/forms.py
@@ -6,23 +6,23 @@ from models import Contact
 #from flask_wtf import Form
 from flask.ext.wtf import Form
 #from wtforms.ext.sqlalchemy.orm import model_form, QuerySelectField, QuerySelectMultipleField
-import wtforms as wtf
+import wtforms
 from wtforms.validators import Required, EqualTo, Email
 from wtforms import validators
-from datetime import datetime
 # from wtforms.fields.html5 import DateField
 
 #ContactForm = model_form(Contact, base_class=Form)
 
-class AddressForm(Form):
-	addr = wtf.StringField(u'Адрес',validators=[validators.Length(max=254)])
+class ContactAddressForm(Form):
+	value = wtforms.StringField(u'Адрес',validators=[Required(), validators.Length(max=254)])
 
 	def __init__(self, csrf_enabled=False, *args, **kwargs):
-		super(FilterForm, self).__init__(csrf_enabled=csrf_enabled, *args, **kwargs)
+		super(ContactAddressForm, self).__init__(csrf_enabled=csrf_enabled, *args, **kwargs)
 
 class ContactForm(Form):
-	lastname = wtf.StringField(u'Фамилия',validators=[validators.Length(max=64)])
-	firstname = wtf.StringField(u'Имя',validators=[Required(),validators.Length(max=64)])
-	midname = wtf.StringField(u'Отчество',validators=[validators.Length(max=64)])
-	birthdate = wtf.DateField(u'Дата рождения')
-	addresses = wtf.FieldList(wtf.FormField(AddressForm))
+	lastname = wtforms.StringField(u'Фамилия',validators=[validators.Length(max=64)])
+	firstname = wtforms.StringField(u'Имя',validators=[Required(),validators.Length(max=64)])
+	midname = wtforms.StringField(u'Отчество',validators=[validators.Length(max=64)])
+	birthdate = wtforms.DateField(u'Дата рождения')
+	addresses = wtforms.FieldList(wtforms.FormField(ContactAddressForm), min_entries=1)
+	#addresses = wtforms.FieldList(wtforms.FormField(ContactAddressForm, default=None), min_entries=1)
