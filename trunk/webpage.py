51c985cd84708b79c19d2de3e86a8707499f1b6f
ti.eugene <ti.eugene@bd074bf9-a489-2ae5-b4c1-63aac862ad04>
2014-02-23 06:26:37 +0000

    webpage.py really ok

--- trunk/webpage.py
+++ trunk/webpage.py
@@ -6,14 +6,10 @@ FIXME:
 * and apps inside
 '''
 
-import urllib
+import urllib, datetime
 from HTMLParser import HTMLParser
 
-# 2. find
-# найти тег
-# выцепить подтег
-# выцепить регексом нужное
-# http://docs.python.org/2/library/htmlparser.html
+DDMMYYYY = '%d.%m.%Y'
 
 def	chk(URL):
 	class	MyHTMLParser(HTMLParser):
@@ -30,91 +26,122 @@ def	chk(URL):
 def	chk_persw():
 	'''
 	PersW
+	States:
+	- 0: not found
+	- 1: found
+	- 9: EOL
 	'''
+	URL = 'http://www.pfrf.ru/ot_peter/soft/5738.html'
 	class	MyHTMLParser(HTMLParser):
-		state = 0	# 0 - not found; 1 - found; 2 - EOL
+		state = 0
 		ver = None
 		date = None
 		def handle_starttag(self, tag, attrs):
-			if (self.state == 0) and (tag == 'title'):
+			if (self.state == 9): return
+			elif (self.state == 0) and (tag == 'title'):
 				self.state = 1
 		def handle_data(self, data):
-			if (self.state == 1):
+			if (self.state == 9): return
+			elif (self.state == 1):
 				if data.startswith(')'):
+					self.state = 9
 					chunks = data.split(' ', 4)[1:4]
 					self.ver = chunks[0].split('.',1)[1]
 					self.date = chunks[2]
-					self.state = 2
-
+					self.date = datetime.datetime.strptime(self.date, DDMMYYYY).date()
 	parser = MyHTMLParser()
-	f = urllib.urlopen('http://www.pfrf.ru/ot_peter/soft/5738.html')
+	f = urllib.urlopen(URL)
 	parser.feed(f.read().decode(f.headers.getparam('charset')))
-	print parser.ver, parser.date
+	return (parser.ver, parser.date)
 
 def	chk_pdspu():
 	'''
-	(ПД СПУ 2010
+	ПД СПУ 2010
+	States:
+	- 0: not found
+	- 1: table
+	- 2: strong
+	- 9: EOL
 	'''
+	URL = 'http://www.pfrf.ru/ot_smolensk_soft/5256.html'
 	class	MyHTMLParser(HTMLParser):
-		state = 0	# 0: not found; 1: table; 2: strong, 3 - EOL
+		state = 0
 		ver = None
 		date = None
 		strongcount = 0
 		def handle_starttag(self, tag, attrs):
-			if (self.state == 0) and (tag == 'table'):
+			if (self.state == 9): return
+			elif (self.state == 0) and (tag == 'table'):
 				self.state = 1
 			elif (self.state == 1) and (tag == 'strong'):
 				self.state = 2
 				self.strongcount += 1
 		def handle_endtag(self, tag):
-			if (self.state == 1) and (tag == 'table'):
-				self.state = 3
-			if (self.state == 2) and (tag == 'strong'):
+			if (self.state == 9): return
+			elif (self.state == 1) and (tag == 'table'):
+				self.state = 9
+			elif (self.state == 2) and (tag == 'strong'):
 				self.state = 1
 		def handle_data(self, data):
-			if (self.state == 2):
+			if (self.state == 9): return
+			elif (self.state == 2):
 				if (self.strongcount == 2):
 					self.ver = data
 				elif (self.strongcount == 4):
 					self.date = data
+					self.date = datetime.datetime.strptime(self.date, DDMMYYYY).date()
 
 	parser = MyHTMLParser()
-	f = urllib.urlopen('http://www.pfrf.ru/ot_smolensk_soft/5256.html')
+	f = urllib.urlopen(URL)
 	parser.feed(f.read().decode(f.headers.getparam('charset')))
-	print parser.ver, parser.date
+	return (parser.ver, parser.date)
 
 def	chk_nd():
 	'''
 	Возмещение НДС: Налогоплательщик
+	States:
+	- 0: not found
+	- 1: found
+	- 9: EOL
 	'''
+	URL = 'http://www.gnivc.ru/software/free_software/software_ul_fl/compensation_vat/'
 	class	MyHTMLParser(HTMLParser):
-		state = 0	# 0 - not found; 1 - found; 2 - EOL
+		state = 0
 		ver = None
 		def handle_starttag(self, tag, attrs):
-			if (self.state == 0) and (tag == 'title'):
+			if (self.state == 9): return
+			elif (self.state == 0) and (tag == 'title'):
 				self.state = 1
 		def handle_data(self, data):
-			if (self.state == 1):
+			if (self.state == 9): return
+			elif (self.state == 1):
+				self.state = 9
 				self.ver = data.rsplit(' ', 1)[1]
-				self.state = 2
-
 	parser = MyHTMLParser()
-	f = urllib.urlopen('http://www.gnivc.ru/software/free_software/software_ul_fl/compensation_vat/')
+	f = urllib.urlopen(URL)
 	parser.feed(f.read().decode(f.headers.getparam('charset')))
-	print parser.ver
+	return (parser.ver, None)
 
 def	chk_kladr():
 	'''
 	КЛАДР
 	<div class="content"> > <div> > after br #2
+	States:
+	- 0: not found
+	- 1: found <div class="content">
+	- 2: found div#2
+	- 3: found br
+	- 9: EOL
 	'''
+	URL = 'http://gnivc.ru/inf_provision/classifiers_reference/kladr/'
 	class	MyHTMLParser(HTMLParser):
-		state = 0	# 0: not found; 1: found div; 2: found div#2, 3: found br, 4: EOL
+		state = 0
 		ver = None
 		date = None
 		brcount = 0
 		def handle_starttag(self, tag, attrs):
-			if (self.state == 4):
+			if (self.state == 9): return
+			elif (self.state == 4):
 				return
 			elif (self.state == 0):
 				if (tag == 'div') and (attrs == [('class', 'content')]):
@@ -126,52 +153,59 @@ def	chk_kladr():
 				if (tag == 'br'):
 					self.brcount += 1
 		def handle_data(self, data):
-			if (self.state == 2):
+			if (self.state == 9): return
+			elif (self.state == 2):
 				if (self.brcount == 1):
 					self.ver = data.split(' ')[1].strip()
 				elif (self.brcount == 2):
+					self.state = 9
 					self.date = data.split('-')[1].strip()
-					self.state = 4
-
+					self.date = datetime.datetime.strptime(self.date, DDMMYYYY).date()
 	parser = MyHTMLParser()
-	f = urllib.urlopen('http://gnivc.ru/inf_provision/classifiers_reference/kladr/')
+	f = urllib.urlopen(URL)
 	parser.feed(f.read().decode(f.headers.getparam('charset')))
-	print parser.ver, parser.date
+	return (parser.ver, parser.date)
 
 def	chk_tconp():
 	'''
 	ТС-Обмен
+	States:
+	- 0: not found
+	- 1: found
+	- 9: EOL
 	'''
+	URL = 'http://www.gnivc.ru/software/free_software/software_ul_fl/cu_np_exchange/'
 	class	MyHTMLParser(HTMLParser):
 		state = 0	# 0 - not found; 1 - found; 2 - EOL
 		ver = None
 		date = None
 		def handle_starttag(self, tag, attrs):
-			if (self.state == 0) and (tag == 'title'):
+			if (self.state == 9): return
+			elif (self.state == 0) and (tag == 'title'):
 				self.state = 1
 		def handle_data(self, data):
-			if (self.state == 1):
+			if (self.state == 9): return
+			elif (self.state == 1):
+				self.state = 9
 				chunks = data.rsplit(' ', 3)[1:]
 				self.ver = chunks[0]
 				self.date = chunks[2]
-				self.state = 2
-
-	# 1. get page
-	#params = urllib.urlencode({'spam': 1, 'eggs': 2, 'bacon': 0})
-	#f = urllib.urlopen("http://www.musi-cal.com/cgi-bin/query", params)
+				self.date = datetime.datetime.strptime(self.date, DDMMYYYY).date()
 	parser = MyHTMLParser()
-	f = urllib.urlopen('http://www.gnivc.ru/software/free_software/software_ul_fl/cu_np_exchange/')
+	f = urllib.urlopen(URL)
 	parser.feed(f.read().decode(f.headers.getparam('charset')))
-	print parser.ver, parser.date
-	# 3. compare with db
-	# 4. store compared
-	# 5. show result
+	return (parser.ver, parser.date)
 
 def	chk_decl2013():
 	'''
 	Декларация 2013
-	# 0: not found; 1: found <div class="section-row">, 2: found <div>, 9 - EOL
+	States:
+	- 0: not found
+	- 1: <div class="section-row">
+	- 2: <div>
+	- 9: EOL
 	'''
+	URL = 'http://www.gnivc.ru/software/free_software/software_fl/ndfl_3_4/'
 	class	MyHTMLParser(HTMLParser):
 		state = 0
 		ver = None
@@ -185,85 +219,101 @@ def	chk_decl2013():
 				self.state = 2
 		def handle_data(self, data):
 			if (self.state == 9): return
-			if (self.state == 2):
+			elif (self.state == 2):
+				self.state = 9
 				chunks = data.strip().rsplit(' ', 3)[1:]
 				self.ver = chunks[0]
 				self.date = chunks[2]
-				self.state = 9
-				return
-
+				self.date = datetime.datetime.strptime(self.date, DDMMYYYY).date()
 	parser = MyHTMLParser()
-	f = urllib.urlopen('http://www.gnivc.ru/software/free_software/software_fl/ndfl_3_4/')
+	f = urllib.urlopen(URL)
 	parser.feed(f.read().decode(f.headers.getparam('charset')))
-	print parser.ver, parser.date
+	return (parser.ver, parser.date)
 
 def	chk_nds():
 	'''
 	Налогоплательщик
+	States:
+	- 0: not found
+	- 1: found
+	- 9: EOL
 	'''
+	URL = 'http://www.nalogy.ru/download.html'
 	class	MyHTMLParser(HTMLParser):
-		state = 0	# 0 - not found; 1 - found; 2 - EOL
+		state = 0
 		date = None
 		def handle_starttag(self, tag, attrs):
-			if (self.state == 0) and (tag == 'div') and (attrs == [('id', 'vers')]):
+			if (self.state == 9): return
+			elif (self.state == 0) and (tag == 'div') and (attrs == [('id', 'vers')]):
 				self.state = 1
 		def handle_data(self, data):
-			if (self.state == 1):
+			if (self.state == 9): return
+			elif (self.state == 1):
+				self.state = 9
 				self.date = data.rstrip()
-				self.state = 2
-				#self.reset()
-
+				y, m, d = self.date.split('.')
+				self.date = datetime.date(int(y), int(m), int(d))
+				#self.date = datetime.datetime.strptime(self.date, DDMMYYYY).date()
 	parser = MyHTMLParser()
-	f = urllib.urlopen('http://www.nalogy.ru/download.html')
+	f = urllib.urlopen(URL)
 	parser.feed(f.read().decode('utf-8'))
-	print parser.date
+	return (None, parser.date)
 
 def	chk_pnv():
 	'''
 	ПНВ
+	States:
+	- 0: not found
+	- 1: <a> found
+	- 2: </a> found
+	- 9: EOL
 	'''
+	URL = 'http://rpn.gov.ru/node/5523'
 	class	MyHTMLParser(HTMLParser):
-		state = 0	# 0 - not found; 1 - <a> found; 2: </a> found>, 9: EOL
+		state = 0
 		ver = None
 		date = None
 		def handle_starttag(self, tag, attrs):
-			if (self.state == 9):
-				return
-			if (self.state == 0) and (tag == 'a') and (attrs == [('id', 'LastVersion'), ('name', 'LastVersion')]):
+			if (self.state == 9): return
+			elif (self.state == 0) and (tag == 'a') and (attrs == [('id', 'LastVersion'), ('name', 'LastVersion')]):
 				self.state = 1
 		def handle_endtag(self, tag):
-			if (self.state == 9):
-				return
+			if (self.state == 9): return
 			elif (self.state == 1) and (tag == 'a'):
 				self.state = 2
 		def handle_data(self, data):
-			if (self.state == 9):
-				return
+			if (self.state == 9): return
 			elif (self.state == 2):
+				self.state = 9
 				chunks = data.split(' ')[1:]
 				self.ver = chunks[0]
 				self.date = chunks[2]
-				self.state = 9
-
+				self.date = datetime.datetime.strptime(self.date, DDMMYYYY).date()
 	parser = MyHTMLParser()
-	f = urllib.urlopen('http://rpn.gov.ru/node/5523')
+	f = urllib.urlopen(URL)
 	parser.feed(f.read().decode(f.headers.getparam('charset')))
-	print parser.ver, parser.date
+	return (parser.ver, parser.date)
 
 def	chk_sbis():
 	'''
 	СБиС
 	<p>Текущая версия программы: <b>2.4.195 от 18.02.2014</b>
+	States:
+	- 0: not found
+	- 1: <p> found
+	- 2: "Текущая версия программы:"
+	- 3: <b> found
+	- 9: EOL
 	'''
+	URL = 'http://sbis.ru/download/update'
 	class	MyHTMLParser(HTMLParser):
-		state = 0	# 0 - not found; 1 - <p> found; 2: "Текущая версия программы:", 3: <b> found, 9: EOL
+		state = 0
 		ver = None
 		ver = None
 		date = None
 		def handle_starttag(self, tag, attrs):
 			if (self.state == 9): return
 			elif (self.state == 0):
-				#print tag
 				if (tag == 'p'):
 					self.state = 1
 			elif (self.state == 2):
@@ -284,17 +334,17 @@ def	chk_sbis():
 				chunks = data.split(' ',)
 				self.ver = chunks[0].strip()
 				self.date = chunks[2].strip()
-
+				self.date = datetime.datetime.strptime(self.date, DDMMYYYY).date()
 	parser = MyHTMLParser()
-	f = urllib.urlopen('http://sbis.ru/download/update')
+	f = urllib.urlopen(URL)
 	parser.feed(f.read().decode(f.headers.getparam('charset')))
-	print parser.ver, parser.date
+	return (parser.ver, parser.date)
 
 def	chk_xml(tofind):
 	'''
 	ChkXML
 	in div class="item2" > <h1>CheckXML+2НДФЛ 2013</h1> - <div class="ver">Дата версии:<br><b>18.02.2014</b></div>
-	states:
+	States:
 	- 0: not found
 	- 1: <div class="item2"> found
 	- 2: <h1> found
@@ -303,6 +353,7 @@ def	chk_xml(tofind):
 	- 5: <b> found
 	- 9: EOL
 	'''
+	URL = 'http://www.buhsoft.ru/?title=download.php'
 	TOFIND = tofind
 	class	MyHTMLParser(HTMLParser):
 		state = 0
@@ -351,21 +402,21 @@ def	chk_xml(tofind):
 				#print "Version:", data
 				self.state = 9
 				self.date = data
-
+				self.date = datetime.datetime.strptime(self.date, DDMMYYYY).date()
 	parser = MyHTMLParser()
-	f = urllib.urlopen('http://www.buhsoft.ru/?title=download.php')
+	f = urllib.urlopen(URL)
 	content = f.read().decode('windows-1251')
 	parser.feed(content)
-	print parser.date
+	return (None, parser.date)
 
 def	chk_chkxml():
-	chk_xml(u'CheckXML')
+	return chk_xml(u'CheckXML')
 
 def	chk_chkxmlufa():
-	chk_xml(u'CheckXML-UFA')
+	return chk_xml(u'CheckXML-UFA')
 
 def	chk_chkxmlndfl():
-	chk_xml(u'CheckXML+2НДФЛ 2013')
+	return chk_xml(u'CheckXML+2НДФЛ 2013')
 
 def	chk_java():
 	'''
@@ -377,6 +428,7 @@ def	chk_java():
 	2: found <strong>
 	9: EOL
 	'''
+	URL = 'http://www.java.com/ru/download/manual.jsp'
 	class	MyHTMLParser(HTMLParser):
 		state = 0
 		ver = None
@@ -394,9 +446,9 @@ def	chk_java():
 				self.ver = chunks[0]+'.'+chunks[2]
 
 	parser = MyHTMLParser()
-	f = urllib.urlopen('http://www.java.com/ru/download/manual.jsp')
+	f = urllib.urlopen(URL)
 	parser.feed(f.read().decode(f.headers.getparam('charset')))
-	print parser.ver
+	return (parser.ver, None)
 
 def	chk_alko():
 	'''
@@ -412,6 +464,7 @@ def	chk_alko():
 	6: found a
 	9: EOL
 	'''
+	URL = 'https://dap.center-inform.ru/tehpod/faq/%D0%A6%D0%B5%D0%BD%D1%82%D1%80+%D0%97%D0%B0%D0%B3%D1%80%D1%83%D0%B7%D0%BA%D0%B8/'
 	class	MyHTMLParser(HTMLParser):
 		state = 0
 		ver = None
@@ -450,22 +503,22 @@ def	chk_alko():
 					self.ver = data.split(' ')[1]
 
 	parser = MyHTMLParser()
-	f = urllib.urlopen('https://dap.center-inform.ru/tehpod/faq/%D0%A6%D0%B5%D0%BD%D1%82%D1%80+%D0%97%D0%B0%D0%B3%D1%80%D1%83%D0%B7%D0%BA%D0%B8/')
+	f = urllib.urlopen(URL)
 	parser.feed(f.read().decode(f.headers.getparam('charset')))
-	print parser.ver
+	return (parser.ver, None)
 
 #chk('https://dap.center-inform.ru/tehpod/faq/%D0%A6%D0%B5%D0%BD%D1%82%D1%80+%D0%97%D0%B0%D0%B3%D1%80%D1%83%D0%B7%D0%BA%D0%B8/')
-#chk_persw()
-#chk_pdspu()
-#chk_nd()
-#chk_kladr()
-#chk_tconp()
-#chk_nds()
-#chk_pnv()
-#chk_decl2013()
-#chk_sbis()
-#chk_chkxml()
-#chk_chkxmlufa()
-#chk_chkxmlndfl()
-#chk_java()
-#chk_alko()
+print 'PersW:\t%s\t%s' %	chk_persw()
+print 'PD SPU:\t%s\t%s' %	chk_pdspu()
+print 'ND:\t%s\t%s' %		chk_nd()
+print 'KLADR:\t%s\t%s' %	chk_kladr()
+print 'TCONP:\t%s\t%s' %	chk_tconp()
+print 'NDS:\t%s\t%s' %		chk_nds()
+print 'PNV:\t%s\t%s' %		chk_pnv()
+print 'Decl2013:\t%s\t%s' %	chk_decl2013()
+print 'SBiS:\t%s\t%s' %		chk_sbis()
+print 'CheckXML:\t%s\t%s' %	chk_chkxml()
+print 'CheckXML-UFA:\t%s\t%s' %	chk_chkxmlufa()
+print 'CheckXML+:\t%s\t%s' %	chk_chkxmlndfl()
+print 'Java:\t%s\t%s' %		chk_java()
+print 'Alko:\t%s\t%s' %		chk_alko()
