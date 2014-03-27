edc70b8bbe4f64a4a87ffaaca04c43f5864bb20f
ti.eugene <ti.eugene@bd074bf9-a489-2ae5-b4c1-63aac862ad04>
2014-03-27 08:13:07 +0000

    flush

--- trunk/dasist/urls.py
+++ trunk/dasist/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 from django.core.urlresolvers import reverse
+from django.contrib.auth.views import login, logout
 
 import pprint
 
@@ -11,8 +12,8 @@ urlpatterns = patterns('',
 	url(r'^admin/',		include(admin.site.urls)),
 	url(r'^admin/jsi18n',	'django.views.i18n.javascript_catalog'), # hack to use admin form widgets
 	#(r'^jsi18n/(?P<packages>\S+?)/$', 'django.views.i18n.javascript_catalog'),
-	url(r'^accounts/login/$',		'django.contrib.auth.views.login'),
-	url(r'^logout$',	'django.contrib.auth.views.logout'),
+	url(r'^accounts/login/$',		login),
+	url(r'^logout$',	logout),
 	url(r'^core/',		include('core.urls')),
 	url(r'^bills/',		include('bills.urls')),
 	url(r'^about$',		'views.about'),
