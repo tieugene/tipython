30f0b03972b54f0a6011415f3253f3dc71812e56
ti.eugene <ti.eugene@bd074bf9-a489-2ae5-b4c1-63aac862ad04>
2014-04-25 06:38:30 +0000

    trunk/dasist to tags/dasist-0.0.2

--- dasist/bills/sql/role.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO "bills_role" VALUES(1,'Исполнитель');
-INSERT INTO "bills_role" VALUES(2,'Начальник ОМТС');
-INSERT INTO "bills_role" VALUES(3,'Руководитель направления');
-INSERT INTO "bills_role" VALUES(4,'Директор');
-INSERT INTO "bills_role" VALUES(5,'Генеральный директор');
-INSERT INTO "bills_role" VALUES(6,'Бухгалтер');
