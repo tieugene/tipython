529f564cf5c689d7304eae237ed8acc9b606ba1b
ti.eugene <ti.eugene@bd074bf9-a489-2ae5-b4c1-63aac862ad04>
2014-04-29 07:04:55 +0000

    flush

--- dasist/2to3.py
+++ dasist/2to3.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 '''
 Converts data from 0.0.2 to 0.0.3
+Src:
+* ./manage.py dumpdata --format=json --indent=1 -a > 0.0.3.json
+Dst:
 * load json
 * handle:
 -- del record
@@ -12,32 +15,47 @@ Converts data from 0.0.2 to 0.0.3
 * save json
 '''
 
-import sys, json
+import sys, json, pprint
 
 '''
-TODEL:
-admin.logentry
-auth.group
-auth.permission
-contenttypes.contenttype
-sessions.session
-sites.site
-bills.state
-
-TOINIT:
-auth.user
-bills.approver
-bills.role
-
-TOSAVE:
-core.file
-core.fileseq
-core.fileseqitem - pk
-scan.event - tojson
-scan.scan  - +comments field
-bills.bill (pk)
-bills.event
-bills.route (-state, -action)
+18/23:
+DEL:
+	admin.logentry
+	auth.group
+	auth.permission
+	bills.state
+	contenttypes.contenttype
+	sessions.session
+	sites.site
+CP:
+	auth.user
+	core.file
+	core.fileseq
+	core.fileseqitem
+	bills.approver
+	bills.role
+ADD:
+	bills.place
+	bills.subject
+	bills.payer
+MODIFY:
+	bills.event:
+		- .comment => char
+	bills.route:
+		-state
+		-action
+	bills.bill:
+		?project
+		+place
+		+subject
+		+payer		= 1
+		+billdate	= 01.01.1970
+		+billno		= '-'
+		+billsum	= 0
+		+payedsum	= 0
+		+topaysum	= 0
+	scan.scan:
+	scan.event: json it
 '''
 
 TOINIT=set([
@@ -46,13 +64,18 @@ TOINIT=set([
 	'bills.role',
 ])
 
+MODELS = set()
+
 def	main(infile):
 	result = list()
 	data = json.load(open(infile))
 	for rec in data:
-		if rec['model'] in TOINIT:
-			result.append(rec)
-	print json.dumps(result, indent=1, encoding="utf-8")
+		#if rec['model'] in TOINIT:
+		#	result.append(rec)
+		if not rec['model'] in MODELS:
+			MODELS.add(rec['model'])
+	pprint.pprint(MODELS)
+	#print json.dumps(result, indent=1, encoding="utf-8")
 
 if __name__ == '__main__':
 	main(sys.argv[1])
